32ef6c88f7e9568f4b027390788cbeb80eaa395b
Ramesh Rai <rameshchamling277@gmail.com>
2024-07-23 18:41:03 +0545

    data parse from one to another activity uisng Intent and intent.getStringExtrat

--- app/src/main/java/com/nmc/ParseActivityTwo.java
+++ app/src/main/java/com/nmc/ParseActivityTwo.java
@@ -3,8 +3,10 @@ package com.nmc;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ParseActivityTwo extends AppCompatActivity {
 
     TextView txtDisplay;
+    EditText txtLname;
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +25,22 @@ public class ParseActivityTwo extends AppCompatActivity {
         Intent intent = getIntent();
         String fname = intent.getStringExtra("fname");
         txtDisplay.setText(fname);
+
+
+        back = findViewById(R.id.btnback);
+        txtLname = findViewById(R.id.txtLname);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String lastname = txtLname.getText().toString();
+
+                intent.putExtra("lastname",fname + " " + lastname);
+                setResult(100,intent);
+                finish();
+            }
+        });
     }
+
+
 }
